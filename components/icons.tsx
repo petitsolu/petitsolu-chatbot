fab905c7a30110466a0b21b8fb1a89baabc383c6
petitsolu <simonicard@gmail.com>
2025-11-13 19:24:17 +0100

    Update icons.tsx

--- components/icons.tsx
+++ components/icons.tsx
@@ -46,6 +46,13 @@ export const CloseIcon = () => (
     </svg>
 );
 
+export const BackIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+        <line x1="19" y1="12" x2="5" y2="12"></line>
+        <polyline points="12 19 5 12 12 5"></polyline>
+    </svg>
+);
+
 export const TrashIcon = () => (
     <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
         <polyline points="3 6 5 6 21 6"></polyline>
